6b13d6e33663f570c6c987ec7dc4132eca1154e3
vqxel <rezq.bagheri@gmail.com>
2025-11-20 11:26:45 -0800

    Updated header

--- js/mm_header.js
+++ js/mm_header.js
@@ -29,7 +29,7 @@ document.write(`
                         <ul class="dropdown-menu" data-bs-theme="light" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" href="./schematic.html">Schematic</a></li>
                             <li><a class="dropdown-item" href="./KiCad.html">KiCad</a></li>
-                            <li hidden><a class="dropdown-item" href="./floodfill.html">Floodfill</a></li>
+                            <li><a class="dropdown-item" href="./floodfill.html">Floodfill</a></li>
                             <li hidden><a class="dropdown-item" href="./MM_simulation.html">Maze Simulation</a></li>
                         </ul>
                     </li>
